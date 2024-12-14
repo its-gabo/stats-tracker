638a9b62d3f54bae61b0e52dd4157f86986ccddd
Gabo <smolana.gabriel@gmail.com>
2024-12-14 22:14:33 +0100

    fix: issue with counting stats

--- src/app/api/stats/per-game/route.ts
+++ src/app/api/stats/per-game/route.ts
@@ -1,4 +1,10 @@
+import { StatsPerGame } from "@/types";
 import { PrismaClient } from "@prisma/client";
+
+type ITotalStats = StatsPerGame & {
+  wins: number;
+  losses: number;
+};
 
 export async function POST(request: Request) {
   const prisma = new PrismaClient();
@@ -29,41 +35,47 @@ export async function POST(request: Request) {
       },
     });
 
-    const currentStats = await prisma.stats.findFirst({
-      where: { player: { id: player.id } },
-    });
-
     const allStats = await prisma.statsPerGame.findMany({
-      where: { playerId: player.id },
+      where: { player: { nickname } },
     });
 
-    const totalKillParticipation = allStats.reduce((acc, curr) => {
-      return acc + curr.killParticipation;
-    }, 0);
+    const totalStats: Omit<ITotalStats, "id" | "createdAt" | "isWin"> =
+      allStats.reduce(
+        (acc, curr) => {
+          return {
+            kills: acc.kills + curr.kills,
+            deaths: acc.deaths + curr.deaths,
+            assists: acc.assists + curr.assists,
+            wins: acc.wins + (curr.isWin ? 1 : 0),
+            losses: acc.losses + (curr.isWin ? 0 : 1),
+            killParticipation: acc.killParticipation + curr.killParticipation,
+          };
+        },
+        {
+          kills: 0,
+          deaths: 0,
+          assists: 0,
+          wins: 0,
+          losses: 0,
+          killParticipation: 0,
+        }
+      );
 
     await prisma.player.update({
       where: { id: player.id },
       data: {
         stats: {
           update: {
-            kills: currentStats!.kills + kills,
-            deaths: currentStats!.deaths + deaths,
-            assists: currentStats!.assists + assists,
-            wins: currentStats!.wins + (isWin ? 1 : 0),
-            losses: currentStats!.losses + (isWin ? 0 : 1),
-            kda:
-              currentStats!.kda === 0
-                ? (kills + assists) / deaths
-                : (currentStats!.kills +
-                    currentStats!.assists +
-                    kills +
-                    assists) /
-                  (deaths + currentStats!.deaths),
-            winRate:
-              ((currentStats!.wins + (isWin ? 1 : 0)) /
-                (currentStats!.wins + currentStats!.losses + 1)) *
-              100,
-            killParticipation: totalKillParticipation / allStats.length,
+            kills: totalStats.kills,
+            deaths: totalStats.deaths,
+            assists: totalStats.assists,
+            wins: totalStats.wins,
+            losses: totalStats.losses,
+            kda: (totalStats.kills + totalStats.assists) / totalStats.deaths,
+            winRate: (totalStats.wins / totalStats.losses) * 100,
+            killParticipation:
+              totalStats.killParticipation / totalStats.wins +
+              totalStats.losses,
           },
         },
       },
